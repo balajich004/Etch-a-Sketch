0789818fbc6191abb1fe116677158de84ebded5e
balajich004 <balajichennupati123@gmail.com>
2024-05-18 11:47:56 +0530

    successfully created the popup and custom grid size functionality

--- scripts/popup.js
+++ scripts/popup.js
@@ -1,3 +1,20 @@
-const setsize=document.querySelector(".grid-szie");
+const setSize=document.querySelector(".grid-size");
 const apply=document.querySelector(".apply-grid-size");
 const close=document.querySelector(".close-popup");
+const popup=document.querySelector(".popup");
+const gridinput=document.getElementById("grid-size-input");
+setSize.addEventListener("click", () => {
+    popup.style.display="flex";
+});
+apply.addEventListener("click", () => {
+    const value=parseInt(gridinput.value);
+    if(value>=1 && value<=100){
+        creategrid(value);
+        popup.style.display="none";
+    }
+    else
+    alert("please enter a number between 1 to 100");
+});
+close.addEventListener("click",()=>{
+    popup.style.display="none";
+})
--- scripts/script.js
+++ scripts/script.js
@@ -1,17 +1,20 @@
-document.addEventListener("DOMContentLoaded",()=>{
-    const container=document.querySelector(".container");
-    const gridinput=document.getElementById("grid-size-input");
-    function creategrid(size){
-        container.innerHTML="";
-        const squareSize = 600 / size;
-        for(let i=0;i<size*size;i++){
-            const square=document.createElement("div");
-            square.classList.add("square");
-            square.style.width=`${squareSize}px`;
-            square.style.height=`${squareSize}px`;
-            container.appendChild(square);
-            square.addEventListener("mouseover",()=>{square.style.backgroundColor="black"});
-        }
+
+const container=document.querySelector(".container");
+function creategrid(size){
+    container.innerHTML="";
+    const squareSize = 600 / size;
+    for(let i=0;i<size*size;i++){
+        const square=document.createElement("div");
+        square.classList.add("square");
+        square.style.width=`${squareSize}px`;
+        square.style.height=`${squareSize}px`;
+        container.appendChild(square);
+        square.addEventListener("mouseover",()=>{square.style.backgroundColor="black"});
     }
+}
+document.addEventListener("DOMContentLoaded",()=>{
     creategrid(16);
 });
+
+
+
