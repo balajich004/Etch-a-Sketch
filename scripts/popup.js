72b56667980794fb6ea3f222889a16537b5cef06
balajich004 <balajichennupati123@gmail.com>
2024-05-18 11:23:21 +0530

    completed the basic idea of the project still to work on custom size pop up

---
+++ scripts/popup.js
@@ -0,0 +1,3 @@
+const setsize=document.querySelector(".grid-szie");
+const apply=document.querySelector(".apply-grid-size");
+const close=document.querySelector(".close-popup");
--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,17 @@
-const container=document.querySelector(".container");
-for(let i=0;i<256;i++){
-        const square=document.createElement("div");
-        square.classList.add("square");
-        square.textContent=i;
-        container.appendChild(square);
-        console.log("yep");
-}
-
+document.addEventListener("DOMContentLoaded",()=>{
+    const container=document.querySelector(".container");
+    const gridinput=document.getElementById("grid-size-input");
+    function creategrid(size){
+        container.innerHTML="";
+        const squareSize = 600 / size;
+        for(let i=0;i<size*size;i++){
+            const square=document.createElement("div");
+            square.classList.add("square");
+            square.style.width=`${squareSize}px`;
+            square.style.height=`${squareSize}px`;
+            container.appendChild(square);
+            square.addEventListener("mouseover",()=>{square.style.backgroundColor="black"});
+        }
+    }
+    creategrid(16);
+});
